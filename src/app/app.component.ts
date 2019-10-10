c597067c27d34d2303c061356aa9d2cd30d967ee
Will <wdahl@albany.edu>
2019-10-10 16:12:56 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'find-restaurants';
+  title = 'Find Restaurants';
+  radius = '1500';
+  type = 'restaurant';
+  key = '&key=AlzaSyDWC7KV-XxyHpDWGlzpV4BjM5iyr2bNM-A';
+  placesLink = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+';
+  city;
+  state;
+  locationList;
+  http: Http;
+  places = [];
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(location): void {
+    this.locationList = location.value.split(', ');
+    this.city = this.locationList[0];
+    this.state = this.locationList[1];
+    this.placesLink = this.placesLink + this.city + ',+' + this.state + this.key;
+    this.http.request(this.placesLink).subscribe((res: Response) => {
+      this.places = res.json().data.candidates;
+    });
+
+  }
 }
